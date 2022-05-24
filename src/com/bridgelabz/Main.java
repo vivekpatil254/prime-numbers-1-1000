412519f877232b0edc68e109b3a9a07d3bdf2b01
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 <Vivekpatil254@gmail.com>
2022-05-24 09:39:40 +0530

    first Commit

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+      //  int array[] = new int[100];
+     //   int prime[] = new int[60];
+        int i, flag;
+      /*  for (i = 0; i < 1000; i++) {
+            array[i] = i + 1;
+        }
+        /*for (i = 0; i < 100; i++) {
+            System.out.println(array[i]);
+        }*/
+
+        int p = 0;
+            for(i=1;i<=1000;i++)  {  //for (i = 1; i < array.length; i++) {
+            int n = i;      //array[i];
+            flag = 0;
+            for (int j = 2; j < n; j++) {
+                if (n % j == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println("prime : "+n);
+       //         prime[p] = n;
+         //       p++;
+            }
+        }
+      /*  System.out.println("prime number from 1 to 100 are : ");
+        for (i = 0; i <= p; i++) {
+            System.out.println(prime[i]);
+        }*/
+    }
+}
